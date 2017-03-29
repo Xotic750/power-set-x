c293690915c95ea7c084b3fca29d5a6d20c55ce1
Xotic750 <xotic750@gmail.com>
2017-03-29 23:34:45 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -24,37 +24,14 @@
  * written P(S), ℘(S), P(S), ℙ(S) or 2S, is the set of all subsets of S,
  * including the empty set and S itself.
  *
- * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
- * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
- * methods that can be faithfully emulated with a legacy JavaScript engine.
+ * Requires ES3 or above.
  *
- * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
- * For these methods, as closely as possible to ES5 is not very close.
- * Many of these shams are intended only to allow code to be written to ES5
- * without causing run-time errors in older engines. In many cases,
- * this means that these shams cause many ES5 methods to silently fail.
- * Decide carefully whether this is what you want. Note: es5-sham.js requires
- * es5-shim.js to be able to work properly.
- *
- * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
- *
- * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
- * behave as closely as possible to ECMAScript 6 (Harmony).
- *
- * @version 1.1.0
+ * @version 1.2.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module power-set-x
  */
-
-/* jslint maxlen:80, es6:true, white:true */
-
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
-   maxstatements:3, maxcomplexity:2 */
 
 /* eslint strict: 1, max-statements: 1 */
 
@@ -66,12 +43,8 @@
 
   var isArrayLike = require('is-array-like-x');
   var isString = require('is-string');
-  var pPush = Array.prototype.push;
-  var pPop = Array.prototype.pop;
-  var pSlice = Array.prototype.slice;
-  var pForEach = Array.prototype.forEach;
-  var sSlice = String.prototype.slice;
-  var pCharAt = String.prototype.charAt;
+  var forEach = require('foreach');
+  var slice = require('lodash._slice');
 
   /**
    * The Power Set method.
@@ -80,32 +53,32 @@
    * @param {Array} value The array like `value` to get the power set of.
    * @return {Array.<Array>} The power set of `value`.
    */
-  var powerSet = function (value) {
+  var pSet = function powerSet(value) {
     var val = [];
     if (isArrayLike(value)) {
       if (value.length < 1) {
-        pPush.call(val, []);
+        val.push([]);
       } else {
         var lastElement;
         var object;
         if (isString(value)) {
-          lastElement = pCharAt.call(value, value.length - 1);
-          object = sSlice.call(value, 0, -1);
+          lastElement = value.charAt(value.length - 1);
+          object = value.slice(0, -1);
         } else {
-          object = pSlice.call(value);
-          lastElement = pPop.call(object);
+          object = slice(value);
+          lastElement = object.pop();
         }
-        pForEach.call(powerSet(object), function (item, index, oSet) {
+        forEach(pSet(object), function (item, index, oSet) {
           var entry = item;
-          pPush.call(val, entry);
-          entry = pSlice.call(entry);
+          val.push(entry);
+          entry = entry.slice();
           oSet[index] = entry;
-          pPush.call(entry, lastElement);
-          pPush.call(val, entry);
+          entry.push(lastElement);
+          val.push(entry);
         });
       }
     } else {
-      pPush.call(val, []);
+      val.push([]);
     }
     return val;
   };
@@ -118,5 +91,5 @@
    * @return {Array.<Array>} The power set of `value`.
    * @see http://en.wikipedia.org/wiki/Power_set
    */
-  module.exports = powerSet;
+  module.exports = pSet;
 }());
